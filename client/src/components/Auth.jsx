d09d4a954c2d5cc22c84f4d4283e963fcd2fe23b
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2022-06-30 16:45:57 +0200

    Final touches

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
 
       const { username, password, phoneNumber, avatarURL } = form;
       console.log(form);
-      const URL = 'http://localhost:5000/auth';
+      const URL = 'https://that-sky-chat.herokuapp.com/auth';
   
       const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
         username, password, fullName: form.fullName, phoneNumber, avatarURL,
